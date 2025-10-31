a5c170afe062dc18c49b0c21dd717132dcc79a9a
Makerfabs <Lin@makerfabs.com>
2025-10-31 18:21:15 +0800

    fix 60hz error

--- firmware/stm32_dimmer/stm32_dimmer.ino
+++ firmware/stm32_dimmer/stm32_dimmer.ino
@@ -1,3 +1,7 @@
+
+#include <Arduino.h>
+#include <HardwareTimer.h>
+
 #define SCR_Pin PA7
 #define RELAY_PIN PC15
 #define LED_PIN PA6
@@ -17,8 +21,27 @@
 
 unsigned char dim = 0;
 
+volatile bool count_hz_flag = false;
+volatile uint32_t count_hz = 0;
+volatile unsigned long zc_samples[10];
+volatile uint8_t zc_index = 0;
+uint8_t dim_delay = 0;
+
+uint32_t local_hz = 0;
+
+HardwareTimer timer(TIM14);
 HardwareSerial mySerial(PB7, PB6);
 int led_flag = 0;
+
+
+
+void onTimer() {
+    if(zc_index < 10 && count_hz > 0) {
+        zc_samples[zc_index++] = count_hz / 2;
+        count_hz = 0;
+    }
+}
+
 
 void setup()
 {
@@ -33,6 +56,11 @@ void setup()
     mySerial.begin(115200);
     delay(1000);
     mySerial.println("Start");
+
+    timer.setOverflow(1000000, MICROSEC_FORMAT);
+    timer.attachInterrupt(onTimer);
+    timer.setInterruptPriority(1, 0);
+    timer.resume();
 
     attachInterrupt(ZCD_PIN, zero_cross_int, RISING); // CHANGE FALLING RISING
 }
@@ -59,6 +87,35 @@ void loop()
         }
         else
             str = str + c;
+    }
+
+
+    if(zc_index == 10) {
+        timer.pause();
+
+        for (uint8_t i = 1; i < zc_index; i++)
+        {
+            unsigned long key = zc_samples[i];
+            int j = i - 1;
+            while (j >= 0 && zc_samples[j] > key)
+            {
+                zc_samples[j + 1] = zc_samples[j];
+                j--;
+            }
+            zc_samples[j + 1] = key;
+        }
+
+        uint32_t sum = 0;
+        for(uint8_t i = 1; i < zc_index - 1; i++) {
+            sum += zc_samples[i];
+        }
+        local_hz = sum / (zc_index - 2);
+
+        if(local_hz > 47 && local_hz < 53) {
+            dim_delay = 100;
+        } else if(local_hz >= 57 && local_hz < 63) {
+            dim_delay = 80;
+        }
     }
 
     delay(1000);
@@ -92,6 +149,10 @@ void set_power(int level)
 void zero_cross_int() // function to be fired at the zero crossing to dim the light
 {
 
+    if(zc_index < 10) {
+        count_hz++;
+    }
+
     if (dim <= DIM_MAX)
         return;
     // if (dim >= DIM_MIN)
@@ -99,7 +160,7 @@ void zero_cross_int() // function to be fired at the zero crossing to dim the li
 
     AC_CTRL_OFF;
 
-    int dimtime = (100 * dim);
+    int dimtime = (dim_delay * dim);
     delayMicroseconds(dimtime); // Off cycle
     AC_CTRL_ON;                 // triac firing
     // delayMicroseconds(500);     // triac On propagation delay
